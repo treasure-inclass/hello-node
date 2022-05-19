e8a1925c8051edb97a4dd8ffeef407ec9e43e82f
treasure-inclass <adrienne.fennell@codeimmersives.com>
2022-05-19 14:08:26 -0400

    Mega final

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("Hello World!")
+console.log("Hello World!");
+
+
+
+
+console.log("HH   HH        lll lll                                     lll      dd !!!");
+console.log("HH   HH   eee  lll lll  oooo   WW      WW   oooo    rr  rr lll      dd !!!");
+console.log("HHHHHHH ee   e lll lll oo  oo  WW   W  WW  oo  oo  rrr   r lll  dddddd !!!");
+console.log("HH  HH   eeee  lll lll oo  oo   WW WW WW   oo  oo  rr      lll dd  dd     ");
+console.log("HH  HH    eeee lll lll  oooo     WW  WW     oooo   rr      lll  ddddd     "); 
